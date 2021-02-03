d650563affca2f45f0da689e678cc1ae3e7d87e2
Zachary McCain <zacmmcca@gmail.com>
2021-02-02 21:57:19 -0600

    Updated cypher queries for complex data storage based on separated nodes of players, playtime, publisher, designer, complexity, rating, and minimum age.

--- database/queries.js
+++ database/queries.js
@@ -29,29 +29,27 @@ const addUserGame = (userId, gameInfo, cb) => {
   // Changes needed:
     // Break Out Nodes:
     /*
-    Designer (relationship: designed)
-    Publisher (relationship: published)
-
-    Players (relationship: playable with)
-    Playtime (relationship: playable in)
     Learn Complexity (relationship: learning curve)
     Strategy Complexity (relationship: strategy complexity)
     User Rating (relationship: user rating)
     Age (relationship: minimum age)
     */
-  const altCypher = `MERGE (a: Designer {name: $primary_designer.name})
+  let players = '' // iterate through values of player numbers and concat
+  let identifierIndex = 112
+  for (let i = gameInfo.min_players; i <= gameInfo.max_players; i++) {
+    players = players + ` MERGE (${String.fromCharCode(identifierIndex)}: Players {number: ${i}})
+    MERGE (${String.fromCharCode(identifierIndex)})-[:PLAYABLE_WITH]->(g) `
+    identifierIndex++
+  }
+
+  const cypher = `MERGE (a: Designer {name: $primary_designer.name})
   MERGE (b: Publisher {name: $primary_publisher.name})
-  MERGE (a)-[:designed]->(g: Game
+  MERGE (a)-[:DESIGNED]->(g: Game
     {
       name: $name,
       id: $id,
       url: $url,
       year_published: $year_published,
-      min_players: $min_players,
-      max_players: $max_players,
-      min_playtime: $min_playtime,
-      max_playtime: $max_playtime,
-      min_age: $min_age,
       description: $description,
       description_preview: $description_preview,
       image_url: $image_url,
@@ -60,39 +58,19 @@ const addUserGame = (userId, gameInfo, cb) => {
       images_medium: $images.medium,
       images_large: $images.large,
       images_original: $images.original,
-      price_US: $msrps[0].price,
-      primary_publisher: $primary_publisher.name,
-      avg_usr_rating: $average_user_rating,
-      primary_designer: $primary_designer.name
+      price_US: $msrps[0].price
     }
-  )<-[:published]-(b)
+  )<-[:PUBLISHED]-(b)
+  MERGE (c: MinPlaytime {number: $min_playtime})-[:PLAYABLE_IN]->(g)
+  MERGE (d: MaxPlaytime {number: $max_playtime})-[:PLAYABLE_IN]->(g)
+  MERGE (e: LearnComplexity {number: ${gameInfo.average_learning_complexity}})-[:LEARNING_CURVE]->(g)
+  MERGE (f: StrategyComplexity {number: ${gameInfo.average_strategy_complexity}})-[:STRATEGY_COMPLEXITY]->(g)
+  MERGE (h: UserRating {number: $average_user_rating})-[:USER_RATING]->(g)
+  MERGE (i: Age {number: $min_age})-[:MIN_AGE]->(g)
+  ${players}
    RETURN g`
-  const cypher = `MERGE (n: Game
-      {
-        name: $name,
-        id: $id,
-        url: $url,
-        year_published: $year_published,
-        min_players: $min_players,
-        max_players: $max_players,
-        min_playtime: $min_playtime,
-        max_playtime: $max_playtime,
-        min_age: $min_age,
-        description: $description,
-        description_preview: $description_preview,
-        image_url: $image_url,
-        images_thumb: $images.thumb,
-        images_small: $images.small,
-        images_medium: $images.medium,
-        images_large: $images.large,
-        images_original: $images.original,
-        price_US: $msrps[0].price,
-        primary_publisher: $primary_publisher.name,
-        avg_usr_rating: $average_user_rating,
-        primary_designer: $primary_designer.name
-      }
-    ) RETURN n`
-  const resultPromise = session.writeTransaction(tx => tx.run(altCypher, gameInfo));
+
+  const resultPromise = session.writeTransaction(tx => tx.run(cypher, gameInfo));
 
   resultPromise.then(result => {
     const singleRecord = result.records[0]
