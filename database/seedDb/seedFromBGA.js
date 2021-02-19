f9026f8d31a37318a13e75e265126dff60300799
Zachary McCain <zacmmcca@gmail.com>
2021-02-18 20:41:38 -0600

    Updated seed file for more complex data.

--- database/seedDb/seedFromBGA.js
+++ database/seedDb/seedFromBGA.js
@@ -10,40 +10,63 @@ const session = driver.session()
 
 const addGameToDatabase = (gameInfo, cb) => {
   let players = ''
+  let minPlayers = ''
+  let maxPlayers = '';
   // console.log('game information: ', gameInfo)
   let identifierIndex = 112
-  for (let i = gameInfo.min_players; i <= gameInfo.max_players; i++) {
-    players = players + ` MERGE (${String.fromCharCode(identifierIndex)}: Players {number: ${i}})
-    MERGE (${String.fromCharCode(identifierIndex)})-[:PLAYABLE_WITH]->(g) `
-    identifierIndex++
+  if (gameInfo.min_players && gameInfo.max_players < 10) {
+    for (let i = gameInfo.min_players; i <= gameInfo.max_players; i++) {
+      players = players + ` MERGE (${String.fromCharCode(identifierIndex)}: Players {number: ${i}})
+      MERGE (${String.fromCharCode(identifierIndex)})-[:PLAYABLE_WITH]->(g) `
+      identifierIndex++
+    }
+    minPlayers = 'min_players: $min_players,'
+    maxPlayers = 'max_players: $max_players,'
   }
   let designer = '';
   let designerRelationship = '';
   let gameDesigner = '';
   if (gameInfo.primary_designer) {
     if (gameInfo.primary_designer.name) {
-      designer = `MERGE (a: Designer {name: $primary_designer.name})`;
-      designerRelationship = `MERGE (a)-[:DESIGNED]->(g)<-[:PUBLISHED]-(b)`;
+      designer = ` MERGE (a: Designer {name: $primary_designer.name})`;
+      designerRelationship = ` MERGE (a)-[:DESIGNED]->(g)`;
       gameDesigner = `, primary_designer: $primary_designer.name`
     }
   }
+  let publisher = '';
+  let publisherRelationship = '';
+  let gamePublisher = '';
+  if (gameInfo.primary_publisher) {
+    if (gameInfo.primary_publisher.name) {
+      publisher = ` MERGE (b: Publisher {name: $primary_publisher.name})`
+      publisherRelationship = `MERGE (g)<-[:PUBLISHED]-(b)`
+      gamePublisher = ` primary_publisher: $primary_publisher.name,`
+    }
+  }
   let year = gameInfo.year_published ? 'year_published: $year_published,' : '';
-  let name = gameInfo.name ? 'name: $name,' : '';
-  let price = gameInfo.msrps ? 'price_US: $msrps[0].price,' : '';
-  let minAge = gameInfo.min_age ? 'min_age: $min_age,' : ''
-  let minPlaytime = gameInfo.min_playtime ? 'min_playtime: $min_playtime,' : '';
-  let maxPlaytime = gameInfo.max_playtime ? 'max_playtime: $max_playtime,' : '';
+  let name = gameInfo.name ? ' name: $name,' : 'name: N/A,';
+  let price = gameInfo.msrps ? ' price_US: $msrps[0].price,' : '';
+  let minAge = gameInfo.min_age ? ' min_age: $min_age,' : ''
+  let minPlaytime = gameInfo.min_playtime ? ' min_playtime: $min_playtime,' : '';
+  let minPlayable = gameInfo.min_playtime ? ' MERGE (c: MinPlaytime {number: $min_playtime}) MERGE(c)-[:PLAYABLE_IN]->(g)' : '';
+  let maxPlaytime = gameInfo.max_playtime ? ' max_playtime: $max_playtime,' : '';
+  let maxPlayable = gameInfo.max_playtime ? ' MERGE (d: MaxPlaytime {number: $max_playtime}) MERGE (d)-[:PLAYABLE_IN]->(g)' : '';
+  let learn = gameInfo.average_learning_complexity ? 'MERGE (e: LearnComplexity {number: $average_learning_complexity}) MERGE (e)-[:LEARNING_CURVE]->(g)' : '';
+  let strategy = gameInfo.average_strategy_complexity ? ' MERGE (f: StrategyComplexity {number: $average_strategy_complexity}) MERGE (f)-[:STRATEGY_COMPLEXITY]->(g)' : ''
+  let age = gameInfo.min_age ? ' MERGE (i: Age {number: $min_age}) MERGE (i)-[:MIN_AGE]->(g)' : ''
+
 
   const cypher =
     `${designer}
-    MERGE (b: Publisher {name: $primary_publisher.name})
+    ${publisher}
+
     MERGE (g: Game {
       ${name}
       id: $id,
       url: $url,
       ${year}
-      min_players: $min_players,
-      max_players: $max_players,
+      ${minPlayers}
+      ${maxPlayers}
       ${minPlaytime}
       ${maxPlaytime}
       ${minAge}
@@ -56,24 +79,21 @@ const addGameToDatabase = (gameInfo, cb) => {
       images_large: $images.large,
       images_original: $images.original,
       ${price}
-      primary_publisher: $primary_publisher.name,
+      ${gamePublisher}
       avg_usr_rating: $average_user_rating
       ${gameDesigner}
     })
     ${designerRelationship}
+    ${publisherRelationship}
     ${players}
-    MERGE (c: MinPlaytime {number: $min_playtime})
-    MERGE(c)-[:PLAYABLE_IN]->(g)
-    MERGE (d: MaxPlaytime {number: $max_playtime})
-    MERGE (d)-[:PLAYABLE_IN]->(g)
-    MERGE (e: LearnComplexity {number: ${gameInfo.average_learning_complexity}})
-    MERGE (e)-[:LEARNING_CURVE]->(g)
-    MERGE (f: StrategyComplexity {number: ${gameInfo.average_strategy_complexity}})
-    MERGE (f)-[:STRATEGY_COMPLEXITY]->(g)
+    ${minPlayable}
+    ${maxPlayable}
+    ${learn}
+    ${strategy}
     MERGE (h: UserRating {number: $average_user_rating})
     MERGE (h)-[:USER_RATING]->(g)
-    MERGE (i: Age {number: $min_age})
-    MERGE (i)-[:MIN_AGE]->(g)
+    ${age}
+
    RETURN g`
 
    return session.writeTransaction(tx => tx.run(cypher, gameInfo))
@@ -90,9 +110,10 @@ const addGameToDatabase = (gameInfo, cb) => {
 }
 
 // Retrieve Board Game Info from Board Game Atlas API
-let searchTerm = 'a'
+let searchTerm = 'd'
+// Completed to 1k items through: ---d---
 
-axios.get(`https://api.boardgameatlas.com/api/search?name=${searchTerm}&client_id=qkHJZ2akQa&limit=25`)
+axios.get(`https://api.boardgameatlas.com/api/search?name=${searchTerm}&client_id=qkHJZ2akQa&limit=1000`)
 // // Reformat Data from BGA
 .then((results) => {
   let i = 0
