ebb637b9014c603c97317ab1e668b37b15e5c7e2
Zachary McCain <zacmmcca@gmail.com>
2021-03-02 12:54:11 -0600

    Updated redundancies.

--- client/modules/Preferences.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/prop-types */
-import React from 'react';
-
-const Preferences = ({ openShelf }) => <h4 id="preferences" onClick={openShelf}>Preferences</h4>;
-
-export default Preferences;
